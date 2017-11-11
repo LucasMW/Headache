f5c8e4285097a61ca0c8d1f3e70043b81b5143dc
LucasMW <great-lucas@hotmail.com>
2017-11-10 23:08:06 -0200

    runnable script draft

--- test/runnable/script.sh
+++ test/runnable/script.sh
@@ -8,7 +8,7 @@ do
 	COUNT=$((COUNT+1))
 	name=$(echo $f | cut -f 1 -d '.')
 	echo "Testing $name"
-	cat $f | ./hac > $f.output
+	./hac $f > $f.output
 	./bfi a.bf > $f.run.output
 	if [ ! -f $name.answer ]; then
     	echo "no answer associated"
