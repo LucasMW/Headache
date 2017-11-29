d9461f555d20cd7c8ecbadcbb55ae0b3916c7af1
LucasMW <great-lucas@hotmail.com>
2017-11-29 16:01:28 -0200

    expander standalone now has modes

--- src/expander.c
+++ src/expander.c
@@ -96,33 +96,61 @@ void cliError(char* prog){
 	printf("Error!\nProgram usage is: \n%s ./expander sourcecode\n",prog);
 	exit(-1);
 }
-
-void run(char* path){
+char* expand(char c, int mode){
+	switch (mode){
+		case 0:
+			return expand8to16NoCopy(c);
+		break;
+		case 1:
+			return expand8to16(c);
+		break;
+		case 2:
+			return expand8to32(c);
+		break;
+		default:
+			return expand8to16NoCopy(c);
+		break;
+	}
+}
+void run(char* path,int mode){
 	FILE* input = fopen(path, "rt");
 	char c;
 	if(!input){
 		input = stdin;
 	}
 	while(fscanf(input,"%c",&c) != EOF){
-			printf("%s", expand8to16NoCopy(c));
+			printf("%s", expand(c,mode));
 	}
 }
+void runStr(char* str, int mode){
+	for(char* p = str;*p;p++) {
+		printf("%s\n", expand(*p,mode));
+	}
+}
+
 int main (int argc, char** argv){
-	//expects ./expander filepath 
-	//expects ./expander -p "bfprogram" 
+	//expects ./expander filepath [1]
+	//expects ./expander -p "bfprogram" [1]
+	char * program;
+	int mode=0;
+	char* option; 
+	if(argc == 4){
+		option = argv[1];
+		program = argv[2];
+		mode = atoi(argv[3]);
+		runStr(program,mode);
+	}
 	if (argc == 3){
 		if(strcmp("-p",argv[1])==0) {
-			for(char* p = argv[2];*p;p++){
-				printf("%s\n", expand8to16NoCopy(*p));
-			}
+			runStr(argv[2],0);
 		} else if(strcmp("-i",argv[1])==0) {
-			run(NULL);
+			run(NULL,mode);
 		} else{
-			cliError(argv[0]);
+			run(argv[1],atoi(argv[2]));
 		}
 	}
 	else if(argc == 2){
-		run(argv[1]);
+		run(argv[1],mode);
 	}
 	else{
 		cliError(argv[0]);
