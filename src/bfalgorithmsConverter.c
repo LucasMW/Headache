b00b4d18004c6d51a1da834cdca69547e123b289
LucasMW <great-lucas@hotmail.com>
2018-01-18 17:10:43 -0200

    utility that helps parsing bf algorithms webpage content

---
+++ src/bfalgorithmsConverter.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <string.h>
+
+static char bfchars[] = {'>','<','+','-',',','.','[',']'};
+static int isBfChar(char c){
+	for(int i=0;i<8;i++){
+		if(c == bfchars[i]){
+			return 1;
+		}
+	}
+	return 0;
+}
+static int zmark = 1002;
+static int ymark = 1001;
+static int xmark = 1000;
+int main() {
+	char c;
+	int n;
+	char string[strlen("temp")];
+	char * p = string;
+	char bigStr[1024*1024]; //1MB
+	int tempList[999]; 
+	int * tempListPtr = tempList;
+	char* bigStrPtr = bigStr;
+	while(fscanf(stdin,"%c",&c)!=EOF){
+		if(!isBfChar(c)) {
+			if(c == 't') {
+				if(fscanf(stdin,"%c",&c)!=EOF){
+					if(c == 'e') {
+						if(fscanf(stdin,"%c",&c)!=EOF){
+							if(c == 'm') {
+								if(fscanf(stdin,"%c",&c)!=EOF){
+									if(c == 'p'){
+										if(fscanf(stdin,"%d",&n)!=EOF){
+											*bigStrPtr = '$';
+											bigStrPtr++;
+											*tempListPtr = n;
+											tempListPtr++;
+											continue;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			else {
+				*bigStrPtr = c;
+				bigStrPtr++;
+				if(c == 'x'){
+					*tempListPtr = xmark;
+					tempListPtr++;
+				} else if (c == 'y'){
+					*tempListPtr = ymark;
+					tempListPtr++;
+				} else if (c == 'z'){
+					*tempListPtr = zmark;
+					tempListPtr++;
+				}
+				continue;
+			}
+		}
+		*bigStrPtr = c;
+		bigStrPtr++;
+	}
+	*bigStrPtr = '\0';
+	*tempListPtr = -1;
+	printf("\nbfalgo(\"%s\"",bigStr);
+	for(tempListPtr = tempList; *tempListPtr > -1; tempListPtr++){
+		
+		if(*tempListPtr == xmark){
+			printf(",x");
+
+		} else if(*tempListPtr == ymark) {
+			printf(",y");
+		}
+		 else if(*tempListPtr == zmark) {
+			printf(",z");
+		}
+		else {
+			printf(",temp%d",*tempListPtr);
+		}
+		
+	}
+	printf(");\n");
+}
