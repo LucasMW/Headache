cbd270b56c482320e2499cc232a435da7b37b978
LucasMW <great-lucas@hotmail.com>
2020-01-12 23:03:25 -0300

    Homebrew tap installation, pt 2

--- HomebrewFormula/headache.rb
+++ HomebrewFormula/headache.rb
@@ -5,7 +5,7 @@ class Headache < Formula
   url "https://github.com/LucasMW/Headache/archive/v0.51.zip"
   sha256 "ec68f8a87b928a9c45741662f8eee5c3c859d02766684d5e1f1d3aac43813b2b"
 
-  depends_on "flex,bison" => :build
+  depends_on "bison" => :build
 
   def install
     make install
