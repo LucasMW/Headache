2541168f4fd92aa50c2749936f13dea249f7a2e4
LucasMW <great-lucas@hotmail.com>
2020-01-12 23:19:12 -0300

    Homebrew tap installation, pt 3

--- HomebrewFormula/headache.rb
+++ HomebrewFormula/headache.rb
@@ -1,13 +1,17 @@
 
 class Headache < Formula
-  desc "A language that compiles to 8 bit brainfuck"
-  homepage "https://github.com/LucasMW/Headache"
+  desc "Programming Language that compiles to 8 Bit Brainfuck"
+  homepage ""
   url "https://github.com/LucasMW/Headache/archive/v0.51.zip"
   sha256 "ec68f8a87b928a9c45741662f8eee5c3c859d02766684d5e1f1d3aac43813b2b"
 
   depends_on "bison" => :build
 
   def install
-    make install
+    system "make install"
+  end
+
+  test do
+    system "make test"
   end
 end
